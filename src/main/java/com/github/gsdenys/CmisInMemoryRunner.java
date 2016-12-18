7fde116029f9d39b68c4775b4d39cfdf04cd0e6d
gsdenys <gsdenys@gmail.com>
2016-12-17 22:38:22 -0200

    Initial commit

---
+++ src/main/java/com/github/gsdenys/CmisInMemoryRunner.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2016 gsdenys@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.gsdenys;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Start a CMIS In Memory with Jetty Server.
+ *
+ * @author Denys G. Santos (gsdenys@gmail.com)
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public class CmisInMemoryRunner extends BlockJUnit4ClassRunner {
+    private static final String LIB_NAME_WAR = "chemistry-opencmis-server-inmemory";
+    private static boolean initialized = false;
+
+    /**
+     * CMIS In Memory Starter
+     *
+     * @param klass The class that will be use to sincronize
+     * @throws InitializationError If any error cour
+     */
+    public CmisInMemoryRunner(Class<?> klass) throws InitializationError {
+        super(klass);
+
+        synchronized (CmisInMemoryRunner.class) {
+            if (!initialized) {
+                System.out.println("Let's run cmis in memory server with jetty ...");
+
+                String filePath = this.getWar();
+
+                try {
+                    initialized = this.startJettyServer(filePath, 8080);
+                } catch (Exception e) {
+                    throw new InitializationError(e);
+                }
+            }
+        }
+    }
+
+    /**
+     * Start jetty server
+     *
+     * @param cmisWar the full path to the cmis in memory war file
+     * @param port    the port to start jetty server
+     * @return boolean
+     * @throws Exception any exception that can occur
+     */
+    private boolean startJettyServer(String cmisWar, Integer port) throws Exception {
+        Server server = new Server(port);
+
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setWar(cmisWar);
+
+        server.setHandler(webapp);
+
+        server.start();
+
+        return true;
+    }
+
+
+    /**
+     * Discovery the full path to the chemistry-opencmis-server-inmemory war file
+     *
+     * @return String
+     * the cmis war file path
+     */
+    private String getWar() {
+        String regex = "(.*:)([a-zA-Z0-9/.-]+" + LIB_NAME_WAR + "[a-zA-Z0-9/.-]*.war)(.*)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(System.getProperty("java.class.path"));
+
+        return matcher.find() ? matcher.group(2) : null;
+    }
+}
---
+++ src/test/java/com/github/gsdenys/CmisInMemoryRunnerTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 gsdenys@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.gsdenys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for the {@link CmisInMemoryRunner} class
+ *
+ * @author Denys G. Santos (gsdenys@gmail.com)
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@RunWith(CmisInMemoryRunner.class)
+public class CmisInMemoryRunnerTest {
+
+    @Test
+    public void name() throws Exception {
+
+            System.out.print("Olá mundo");
+    }
+
+    @Test
+    public void name1() throws Exception {
+        URL url = new URL("http://localhost:8080/atom");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+
+        if (conn.getResponseCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + conn.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                (conn.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        conn.disconnect();
+    }
+}
